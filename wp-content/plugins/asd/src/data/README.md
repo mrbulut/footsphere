9c63c733810baaa86db947bb8709488e23768743
mrbulut <miksmtr@gmail.com>
2019-02-07 16:50:51 +0300

    go go

--- wp-content/plugins/asd/src/data/README.md
+++ wp-content/plugins/asd/src/data/README.md
@@ -1,8 +1,8 @@
 
-<code> $database = new CustomerDal(); </code>
 
-///
-use in all database object 
+# Use in all database object 
+<code> $database = new CustomerDal(); </code>
+---------
 <pre><code>
 $addObject = new Customer();
 $whereObject = new Customer();
@@ -11,8 +11,31 @@ $whereObject->setID("1");
 
 $database->settingQuery($addObject,$whereObject); // make input and query
 $database->insertToObject(); // return true or false
-$database->getToObject();    // return the object
+$database->getToObject();    // return the array
 $database->updateToObject(); // return true or false
 $database->deleteToObject(); // return true or false
 </pre></code>
-///
+
+https://github.com/ThingEngineer/PHP-MySQLi-Database-Class
+You can use features in This github link with $database object 
+---------
+# class CustomerDal; 
+<pre><code>
+$database = new CustomerDal(); 
+$database->getProductWaitingCustomers(); // return array
+$database->getProductNoCompoleteCustomers(); // return not compolete users 
+$database->getProductCompoleteCustomers(); // return compolete users
+$database->getProductFixCustomers(); // return ready users
+
+</code></pre>
+-----------
+# class MessageDal; 
+<pre><code>
+$database = new MessageDal(); 
+$database->getAllMessagesToUserId($Id,$HowManyMessage); 
+$database->getTotalMessage($HowManyMessage); // messages in all of system 
+$database->getUnreadMessages($HowManyMessage); // messages in all of system 
+$database->isThereAnyUnreadMessage($HowManyMessage); // unread message for the user
+
+</code></pre>
+--------------
--- wp-content/plugins/asd/src/data/concrete/MessageDal.php
+++ wp-content/plugins/asd/src/data/concrete/MessageDal.php
@@ -21,21 +21,6 @@ class MessageDal extends DatabaseTableDao implements IDatabaseTableDao
     }
 
 
-    // Gonderilen id degerine sahip satırı geri döndürürr.
-    public function getMessageToId($id)
-    {
-        if ($id) {
-            return $this->select(
-                array(
-                    $this->Rows[0] => $id
-                )
-            );
-        } else
-            return false;
-    }
-
-    // UserId nin kayıtlı olduğu tüm mesajları döndürür
-    // $HowManyMessage değişkeni kaç adet mesaj istediğini temsil eder.
     public function getAllMessagesToUserId($UserId, $HowManyMessage = null)
     {
         if ($UserId) {
@@ -48,7 +33,6 @@ class MessageDal extends DatabaseTableDao implements IDatabaseTableDao
             return false;
     }
 
-    // Sistemdeki toplam atılan mesajları döndürür.
     public function getTotalMessage($HowManyMessage = null)
     {
         return $this->selectAll(array(), $HowManyMessage);
@@ -64,38 +48,7 @@ class MessageDal extends DatabaseTableDao implements IDatabaseTableDao
             , $HowManyMessage);
     }
 
-    // Kullanıcının mesajını görüldü yap.UserId
-    public function makeMessagesViewedToUserId($UserId)
-    {
-        if ($UserId) {
-            return $this->update(
-                array(
-                    $this->Rows[5] => 'Okunmus'
-                ),
-                array(
-                    $this->Rows[1] => $UserId
-                )
-            );
-        } else
-            return false;
-    }
 
-    // Mesaj yazma
-    public function writeMessage(Message $message)
-    {
-        if ($message) {
-            return $this->insert(
-                array(
-                    $this->Rows[3] => $message->Message,
-                    $this->Rows[2] => $message->WhoIsMessage,
-                    $this->Rows[4]=> $message->Date,
-                    $this->Rows[5] => $message->Status,
-                    $this->Rows[1] => $message->UserId,
-                )
-            );
-        } else
-            return false;
-    }
 
     //
     public function isThereAnyUnreadMessage($UserId, $HowManyMessage = null){
@@ -111,17 +64,6 @@ class MessageDal extends DatabaseTableDao implements IDatabaseTableDao
             return false;
     }
 
-    public function usersLastMessageDate($UserId){
-        if ($UserId) {
-            $list =  $this->selectAll(
-                array(
-                    'UserId' => $UserId
-                )
-            );
-            return $list[count($list)-1]['Date'];
-        } else
-            return false;
-    }
 
 
 
